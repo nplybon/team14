0517b7b80c58a436781de51dab4b9a3e1f3fcbb8
ajwalker <ajwalker@10.116.8.128>
2021-03-30 14:42:17 -0400

    Commit Calculate Class Test

---
+++ rimplex/testing/CalculateTest.java
@@ -0,0 +1,24 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import util.Calculate;
+import util.Expression;
+
+class CalculateTest
+{
+
+  @Test
+  void testCalculateConstructor() {
+    
+  }
+  
+  @Test
+  void testCalculateAddition()
+  {
+    fail("Not yet implemented");
+  }
+
+}
