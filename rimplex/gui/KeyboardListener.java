4898b1aa128f5a86b2060f3e5ecd7d69d9ae937f
coltonshovlin <shovlicj@dukes.jmu.edu>
2021-04-10 14:10:39 -0400

    implement actionListener

--- rimplex/gui/KeyboardListener.java
+++ rimplex/gui/KeyboardListener.java
@@ -1,11 +1,13 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JOptionPane;
 
-public class KeyboardListener implements KeyListener {
+public class KeyboardListener implements KeyListener, ActionListener {
 
 	private static KeyboardListener listener;
 	
@@ -92,4 +94,10 @@ public class KeyboardListener implements KeyListener {
 	        
 	    return listener;  
 	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
