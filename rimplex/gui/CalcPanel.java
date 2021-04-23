80d5cc414ab71b0b41a5d138570187c11c68f4f8
ajwalker <ajwalker@10.116.8.128>
2021-04-23 14:07:09 -0400

    Alex - Implemented sign change

--- rimplex/gui/CalcPanel.java
+++ rimplex/gui/CalcPanel.java
@@ -266,7 +266,7 @@ public class CalcPanel extends Panel {
 		inverse.setEnabled( false );
 		log.setEnabled( false );
 		exponent.setEnabled( false );
-		sign.setEnabled( false );
+		//sign.setEnabled( false );
 		closeHistory.setEnabled( false );
 		squareRoot.setEnabled( false );
 		closePar.setEnabled(false);
@@ -569,5 +569,13 @@ public class CalcPanel extends Panel {
 	
 	public void dPresentTrue() {
 	  dPresent = true;
+	}
+	
+	public void changeSign() {
+	  if (display.getText().charAt(0) == '-') {
+	    display.setText(display.getText().substring(1));
+	  } else {
+	    display.setText("-" + display.getText()); 
+	  }
 	}
 }
