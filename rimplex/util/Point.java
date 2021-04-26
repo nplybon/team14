4debf7adc23c190d89e86ac9a3af7711fa4bea84
hmann <mann3he@dukes.jmu.edu>
2021-04-26 11:34:26 -0400

    Point.java

---
+++ rimplex/util/Point.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Point {
+	
+	private double a;
+	private double b;
+	
+	public Point(double a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public double getA() {
+		return this.a;
+	}
+	
+	public double getB() {
+		return this.b;
+	}
+
+}
